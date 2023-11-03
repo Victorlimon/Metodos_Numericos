3fd5d091de40176079679ecf8dd2ce310e95374f
Victorlimon <victorlimon.xc@gmail.com>
2023-11-03 00:22:58 -0400

    Proyecto Metodos Numericos

---
+++ 1-MetodoBiseccion.py
@@ -0,0 +1,41 @@
+error = 1E-6 #0.0000001
+
+#Entradas 
+import sympy as sp
+from math import e
+funcion =  "2.718281828459045**x + log(x) - 5.1 "  #"(x-3)**3/6-(x-2)**2/4+x-5"
+ec = sp.sympify(funcion)  #sympify.-estamos convirtiendo la cadena de expresión en una expresión matemática real 
+x = sp.symbols("x")      #podemos declarar algunas variables para el uso de expresiones matemáticas y polinomios
+x0 = 1
+x1 = 10
+print("Ecuacion = ", ec)
+print("x0 = ", x0)
+print("x1 = ", x1)
+
+#Proceso
+f0 = ec.subs({x: x0}).evalf()
+f1 = ec.subs({x0: x1}).evalf()
+f2 = 1E10
+auxi = 0
+while abs(f2) > error:    # abs.-valor absolto
+    auxi = auxi + 1
+    x2 = (x0+x1)/2
+    print("\n Iteracion: ", auxi)
+    print("x0 = ", x0)
+    print("f(x0) = ", f0)
+    print("x1 = ", x1)
+    print("f(x1) = ", f1)
+    print("x2 = ", x2)
+    print("f(x2) = ", f2)
+    f0 = ec.subs({x: x0}).evalf()
+    f2 = ec.subs({x: x2}).evalf()
+    f1 = ec.subs({x: x1}).evalf()
+    if ec.subs({x: x0}).evalf() * ec.subs({x: x2}).evalf() < 0:
+        x1 = x2
+    elif ec.subs({x: x1}).evalf() * ec.subs({x: x2}).evalf() < 0:
+        x0 = x2
+    else:
+        x2 = x2
+        
+print("\n\n x = ", x2)
+print("Iteraciones: ", auxi)
